58092e4c541075325c5003601861a1b10ce42760
João Vitor Espig <jotinha300@gmail.com>
2021-12-05 00:40:21 -0300

    Main updated, now it's using GetFileStats

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Println("File transfer by Jota")
+	fmt.Print("File transfer by Jota\n\n")
 	fileContent, err := file.ReadFile("whom.txt")
 	if err != nil {
 		err = file.CreateAndWriteFile("whom.txt", "Vai tomar no cu seu corno do caralho")
@@ -19,5 +19,10 @@ func main() {
 			log.Fatalln(err)
 		}
 	}
-	fmt.Println(fileContent)
+	fileStats, err := file.GetFileStats("whom.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Printf("Name: %v\nSize: %v\nPermissions: %v\n", fileStats.Name(), fileStats.Size(), fileStats.Mode())
+	fmt.Printf("Content: %v\n", fileContent)
 }
