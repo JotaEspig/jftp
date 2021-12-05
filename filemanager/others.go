14afc29413add35e936f2693257dde4d0cdbf5df
João Vitor Espig <jotinha300@gmail.com>
2021-12-05 00:24:48 -0300

    Added functions to Copy and rename a file

--- filemanager/others.go
+++ filemanager/others.go
@@ -6,3 +6,15 @@ func DeleteFile(filename string) error {
 	err := os.Remove("test.txt")
 	return err
 }
+
+func CopyFile(filenameToCopy string, newFilename string) error {
+	content, err := ReadFile(filenameToCopy)
+	if err != nil {
+		return err
+	}
+	return CreateAndWriteFile(newFilename, string(content))
+}
+
+func RenameFile(filename string, newFilename string) error {
+	return os.Rename(filename, newFilename)
+}
